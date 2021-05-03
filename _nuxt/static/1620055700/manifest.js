2b0ddc69ad844c38b38d88461e8271359b4831b3
Joaquin Prado <jprado@standardshub.io>
2021-05-03 08:34:28 -0700

    Updates

--- _nuxt/static/1620055700/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002Ffetching","\u002Finstallation","\u002Fdisplaying","\u002Fintegrations","\u002Fconfiguration","\u002Fadvanced","\u002Fsetup","\u002Fsnippets","\u002Fwriting","\u002Fexamples\u002Ftailwindcss-typography","\u002Fthemes\u002Fdocs","\u002Fexamples\u002Fdocs-theme","\u002Fexamples\u002Fbasic"]})
---
+++ _nuxt/static/1620056036/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002Finstallation","\u002Fconfiguration","\u002Ffetching","\u002Fadvanced","\u002Fintegrations","\u002Fsetup","\u002Fsnippets","\u002Fdisplaying","\u002Fwriting","\u002Fexamples\u002Fbasic","\u002Fexamples\u002Fdocs-theme","\u002Fthemes\u002Fdocs","\u002Fexamples\u002Ftailwindcss-typography"]})
