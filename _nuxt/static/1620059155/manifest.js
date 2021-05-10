b99bd78e9e52c340b325be3918a2739d2dce719a
Joaquin Prado <jprado@standardshub.io>
2021-05-09 17:48:02 -0700

    Updates

--- _nuxt/static/1620059155/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Freleases","\u002F","\u002Finstallation","\u002Fadvanced","\u002Fintegrations","\u002Fdisplaying","\u002FREADME","\u002Ffetching","\u002Fconfiguration","\u002Fsnippets","\u002Fwriting","\u002Fexamples\u002Fbasic","\u002Fexamples\u002Fdocs-theme","\u002Fthemes\u002Fdocs","\u002Fexamples\u002Ftailwindcss-typography"]})
