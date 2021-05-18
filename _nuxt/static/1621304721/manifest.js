22602b18e4162a8639985c3eda65540121008c42
LADY VARGAS <lady.vargas@pucese.edu.ec>
2021-05-17 21:38:22 -0500

    Updates

--- _nuxt/static/1621304721/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002Fdisplaying","\u002Finstallation","\u002Fadvanced","\u002Fwriting","\u002Ffetching","\u002Fconfiguration","\u002Fintegrations","\u002FREADME","\u002Fsnippets","\u002Fthemes\u002Fdocs","\u002Fexamples\u002Ftailwindcss-typography","\u002Fexamples\u002Fdocs-theme","\u002Fexamples\u002Fbasic"]})
---
+++ _nuxt/static/1621305444/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002Fconfiguration","\u002Finstallation","\u002Fadvanced","\u002FREADME","\u002Ffetching","\u002Fwriting","\u002Fdisplaying","\u002Fintegrations","\u002Fsnippets","\u002Fthemes\u002Fdocs","\u002Fexamples\u002Fdocs-theme","\u002Fexamples\u002Fbasic","\u002Fexamples\u002Ftailwindcss-typography"]})
