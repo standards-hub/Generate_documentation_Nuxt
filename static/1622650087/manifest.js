328e8b36d5270987158ddfe19365bde3773f943a
LadyVargas <45071201+ladyvargas@users.noreply.github.com>
2021-06-02 11:16:12 -0500

    Add files via upload

---
+++ static/1622650087/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Freleases","\u002F","\u002Fsetup","\u002Fexamples\u002Fbasic","\u002Fcomponentes","\u002Fexamples\u002Fdocs-theme","\u002FREADME","\u002Fexamples\u002Ftailwindcss-typography"]})
