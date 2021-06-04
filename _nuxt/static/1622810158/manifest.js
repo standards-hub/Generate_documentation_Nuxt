d6c22f867cf0d41ed751684bafbc658b3131df26
ladyvargas <lady.vargas.santana@gmail.com>
2021-06-04 08:12:28 -0500

    Updates

---
+++ _nuxt/static/1622810158/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002Fcomponentes","\u002Fsetup","\u002Fexamples\u002Ftailwindcss-typography","\u002Fexamples\u002Fdocs-theme","\u002Fexamples\u002Fbasic"]})
